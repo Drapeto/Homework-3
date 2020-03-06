9f49a698ecb416697f1533c01858edb5aac90d9e
Drapeto <drapetorres@gmail.com>
2020-03-05 22:31:59 -0700

    Complete Assignment

--- script.js
+++ script.js
@@ -6,20 +6,37 @@ var passwordText = document.getElementById("password");
 // Write password to the #password input/write all code here for the button
 function writePassword() {
   var length = Number(prompt("Type the amount of characters you want between 8 and 128"))
-  var upperCase = confirm("Do you want uppercase characters?")
-  var charset="";
-  if (upperCase) {
-    charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+  if (length<8 || length>128){
+
+    alert("Password must contain at least 8 charaters and no more than 128.")
   }
-  var lowerCase = confirm("Do you want lowercase characters?")
-  if (lowerCase) {
-    charset += "abcdefghijklmnopqrstuvwxyz"
+  else{
+
+    var upperCase = confirm("Do you want uppercase characters?")
+    var charset = "";
+    if (upperCase) {
+      charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
+    var lowerCase = confirm("Do you want lowercase characters?")
+    if (lowerCase) {
+      charset += "abcdefghijklmnopqrstuvwxyz"
+    }
+    var symbols = confirm("Do you you want to inlcude symbols?")
+    if (symbols) {
+      charset += "!@#$%^&*()_+"
+    }
+    var numbers = confirm("Do you you want to inlcude numbers?")
+    if (numbers) {
+      charset += "1234567890"
+    }
   }
-  // charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+
   retVal = "";
   for (var i = 0, n = charset.length; i < length; ++i) {
     retVal += charset.charAt(Math.floor(Math.random() * n));
   }
+ 
   passwordText.value = retVal
 }
 
